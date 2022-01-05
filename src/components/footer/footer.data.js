0af70dd65cd894725cb0ad5eb42d9b0e39d2e964
Flavianooo <flavianosamp@gmail.com>
2022-01-05 21:06:44 +0300

    Initial commit

---
+++ src/components/footer/footer.data.js
@@ -0,0 +1,61 @@
+export default [
+  {
+    header: 'Hakkımızda',
+    items: [
+      {
+        path: '/',
+        label: 'Destek Merkezi',
+      },
+      {
+        path: '/',
+        label: 'Müşteri Merkezi',
+      },
+      {
+        path: '/',
+        label: 'Hakkımızda Bilgiler',
+      },
+      {
+        path: '/',
+        label: 'Telif Hakları',
+      }
+    ],
+  },
+  {
+    header: 'Bilgilerimiz',
+    items: [
+      {
+        path: '/',
+        label: 'Gizlilik Politikamız ',
+      },
+      {
+        path: '/',
+        label: 'Koşul ve Şartlarımız',
+      },
+      {
+        path: '/',
+        label: 'Site Haritası',
+      },
+      {
+        path: '/',
+        label: 'Bulunduğumuz Yer',
+      }
+    ],
+  },
+  {
+    header: 'Politikalarımız',
+    items: [
+      {
+        path: '/',
+        label: 'Ürün Güvenliği',
+      },
+      {
+        path: '/',
+        label: 'Yazılım Prensipleri',
+      },
+      {
+        path: '/',
+        label: 'İstenmeyen Yazılım Politikası',
+      }
+    ],
+  },
+];
---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'Anasayfa',
+  },
+  {
+    path: 'feature',
+    label: 'Ürünlerimiz',
+  },
+  {
+    path: 'testimonial',
+    label: 'Hakkımızda',
+  },
+  {
+    path: 'pricing',
+    label: 'Fiyatlandırmalar',
+  },
+];
